99dc361cc125a8ee2bc6c05b3c81bb9f381d1db3
Joe Callahan <joecallahan@macbookair.lan>
2025-01-21 10:39:56 -0800

    starting project

---
+++ index.js
@@ -0,0 +1,5 @@
+const userNumbers = prompt ('Please input numbers.');
+const userNumbersAsString = userNumbers.split(`,`);
+const userNumbersAsNumbers = [];
+
+for (const indexCounter = 0; indexCounter < userNumbersAsString.length; indexCounter = indexCounter + 1);
